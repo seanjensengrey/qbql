dcdd7974c1aff80ef07e431bb1e07db032e7008a
vadimtro <vadimtro@3fa168a8-ab54-0410-bc16-1f818b10f66c>
2008-12-09 20:08:54 +0000

    Reformatted with tab changed to spaces

--- trunk/qbql/lattice/Tuple.java
+++ trunk/qbql/lattice/Tuple.java
@@ -1,63 +1,63 @@
 package qbql.lattice;
 
 class Tuple implements Comparable {
-	String[] data;
+    String[] data;
 
-	public Tuple( String[] data ) {
-		super();
-		this.data = data;
-	}
+    public Tuple( String[] data ) {
+        super();
+        this.data = data;
+    }
 
-	// careful: this works in the scope of one relation only, 
-	// not between different relations
-	// see the overloaded equals( Object obj, Relation x, Relation y )
-	public boolean equals( Object obj ) {
-		Tuple src = (Tuple) obj;
-		if( src.data.length != data.length )
-			return false;
-		for( int i = 0; i < data.length; i++ )
-			if( !src.data[i].equals(data[i]) )
-				return false;
-		return true;
-	}
-	/**
-	 * @param obj
-	 * @param x -- need to map named perspective into positional
-	 * @param y -- the order of x and y doesn't matter
-	 * @return
-	 */
-	public boolean equals( Object obj, Relation x, Relation y ) {
-		Tuple src = (Tuple) obj;
-		if( src.data.length != data.length )
-			return false;
-		for( int i = 0; i < data.length; i++ ) {
-			String colName = x.colNames[i];
-			int j = y.header.get(colName);
-			if( !src.data[i].equals(data[j]) )
-				return false;
-		}
-		return true;
-	}
+    // careful: this works in the scope of one relation only, 
+    // not between different relations
+    // see the overloaded equals( Object obj, Relation x, Relation y )
+    public boolean equals( Object obj ) {
+        Tuple src = (Tuple) obj;
+        if( src.data.length != data.length )
+            return false;
+        for( int i = 0; i < data.length; i++ )
+            if( !src.data[i].equals(data[i]) )
+                return false;
+        return true;
+    }
+    /**
+     * @param obj
+     * @param x -- need to map named perspective into positional
+     * @param y -- the order of x and y doesn't matter
+     * @return
+     */
+    public boolean equals( Object obj, Relation x, Relation y ) {
+        Tuple src = (Tuple) obj;
+        if( src.data.length != data.length )
+            return false;
+        for( int i = 0; i < data.length; i++ ) {
+            String colName = x.colNames[i];
+            int j = y.header.get(colName);
+            if( !src.data[i].equals(data[j]) )
+                return false;
+        }
+        return true;
+    }
 
-	public int hashCode() {
-		if( data.length == 0 )
-			return 0;
-		else
-			return data[0].hashCode();
-	}
+    public int hashCode() {
+        if( data.length == 0 )
+            return 0;
+        else
+            return data[0].hashCode();
+    }
 
-	public int compareTo( Object o ) {
-		Tuple src = (Tuple) o;
-		return toString().compareTo(src.toString());
-	}
+    public int compareTo( Object o ) {
+        Tuple src = (Tuple) o;
+        return toString().compareTo(src.toString());
+    }
 
-	public String toString() {
-		StringBuffer ret = new StringBuffer("<");
-		for( int i = 0; i < data.length; i++ )
-			ret.append(data[i]+",");
-		ret.append(">");
-		return ret.toString();
-	}
-	
-	
+    public String toString() {
+        StringBuffer ret = new StringBuffer("<");
+        for( int i = 0; i < data.length; i++ )
+            ret.append(data[i]+",");
+        ret.append(">");
+        return ret.toString();
+    }
+
+
 }
