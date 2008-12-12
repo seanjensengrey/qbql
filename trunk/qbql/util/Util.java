cac99a20ef68a1158fbb519bc302fb59b9d28f32
vadimtro <vadimtro@3fa168a8-ab54-0410-bc16-1f818b10f66c>
2008-12-12 00:10:56 +0000

    Minor changes; reformatted parser and util.

--- trunk/qbql/util/Util.java
+++ trunk/qbql/util/Util.java
@@ -9,69 +9,69 @@ import java.net.URL;
 import qbql.lattice.Relation;
 
 public abstract class Util {
-	public static void main(String[] args) throws Exception {
-		/*Set<RuleTuple> rules = ParserEngine.extractRules();
+    public static void main(String[] args) throws Exception {
+        /*Set<RuleTuple> rules = ParserEngine.extractRules();
 		Set<String> keywords = ParserEngine.getKeywords(rules);
 		for( String s : keywords )
 			System.out.println("| "+s);  // (authorized)
-		 */
-		int p = decrPair(pair(16,57));
-		System.out.println("x= "+X(p)+", y= "+Y(p));  // (authorized)
-	}
+         */
+        int p = decrPair(pair(16,57));
+        System.out.println("x= "+X(p)+", y= "+Y(p));  // (authorized)
+    }
 
-	public static String readFile( String file ) throws Exception {
-		return readFile(new FileInputStream(file));
-	}
+    public static String readFile( String file ) throws Exception {
+        return readFile(new FileInputStream(file));
+    }
 
-	public static String readFile( Class c, String file ) throws Exception {
+    public static String readFile( Class c, String file ) throws Exception {
         URL u = c.getResource( file );
-		return readFile(u.openStream());
-	}
-	
- 	public static String readFile(InputStream is) throws Exception {
-		byte[] bytes = new byte[4096];
-		int bytesRead = 0;
-		BufferedInputStream bin = null;
-		StringBuffer ret = new StringBuffer();
-		try {
-			bin = new BufferedInputStream(is);
-			bytesRead = bin.read(bytes, 0, bytes.length);
-			while (bytesRead != -1) {
-				ret.append(new String(bytes).substring(0, bytesRead));
-				bytesRead = bin.read(bytes, 0, bytes.length);
-			}
-		} finally {
-			if (bin != null)
-				bin.close();
-		}
-		return ret.toString();
-	}
+        return readFile(u.openStream());
+    }
 
-	public static String toNull( String src ) {
-		return "".equals(src)? null : src;
-	}
+    public static String readFile(InputStream is) throws Exception {
+        byte[] bytes = new byte[4096];
+        int bytesRead = 0;
+        BufferedInputStream bin = null;
+        StringBuffer ret = new StringBuffer();
+        try {
+            bin = new BufferedInputStream(is);
+            bytesRead = bin.read(bytes, 0, bytes.length);
+            while (bytesRead != -1) {
+                ret.append(new String(bytes).substring(0, bytesRead));
+                bytesRead = bin.read(bytes, 0, bytes.length);
+            }
+        } finally {
+            if (bin != null)
+                bin.close();
+        }
+        return ret.toString();
+    }
 
-	public static String identln( int level, String txt ) {
-		StringBuffer b = new StringBuffer();
-		for(int i = 0; i< level;i++)
-			b.append(" "); 
-		b.append(txt); 
-		return b.toString();
-	}
+    public static String toNull( String src ) {
+        return "".equals(src)? null : src;
+    }
 
-	// interval indexes
-	public static int pair( int x, int y ) {
-		return (y<<16)|x;
-	}
-	public static int Y( int p ) {
-		return p>>16;
-	}
-	public static int X( int p ) {
-		return p&0xffff;
-	}
-	// x--; y--
-	public static int decrPair( int p ) {
-		return p-0x10001;
-	}
+    public static String identln( int level, String txt ) {
+        StringBuffer b = new StringBuffer();
+        for(int i = 0; i< level;i++)
+            b.append(" "); 
+        b.append(txt); 
+        return b.toString();
+    }
+
+    // interval indexes
+    public static int pair( int x, int y ) {
+        return (y<<16)|x;
+    }
+    public static int Y( int p ) {
+        return p>>16;
+    }
+    public static int X( int p ) {
+        return p&0xffff;
+    }
+    // x--; y--
+    public static int decrPair( int p ) {
+        return p-0x10001;
+    }
 
 }
