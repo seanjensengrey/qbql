44870e3fb0754304612e563795a4c17a4dc83091
vadimtro <vadimtro@3fa168a8-ab54-0410-bc16-1f818b10f66c>
2008-12-12 00:10:56 +0000

    Minor changes; reformatted parser and util.

--- qbql/parser/Matrix.java
+++ qbql/parser/Matrix.java
@@ -8,37 +8,37 @@ import qbql.util.Util;
  * Main Data Structure for CYK method
  */
 public class Matrix extends TreeMap<Integer,int[]> {
-	private CYK cyk;
-	
-	public Matrix( CYK cyk ) {
-		this.cyk = cyk;
-	}
+    private CYK cyk;
 
-	public boolean contains( int x, int y, int symbol ) {
-		int[] js = get(Util.pair(x, y));
-		if( js == null )
-			return false;
-		for( int ii : js )
-			if( Util.X(ii) == symbol )
-				return true;
-		return false;
-	}
+    public Matrix( CYK cyk ) {
+        this.cyk = cyk;
+    }
 
-	public int getSymbol( int interval, int index ) {
-		int[] tmp = get(interval);
-		if( tmp == null )
-			return -1;
-		return Util.X(tmp[index]);
-	}
-	
-	public int getIntervalMiddle( int interval, int index ) {
-		int[] tmp = get(interval);
-		if( tmp == null )
-			return -1;
-		return Util.Y(tmp[index]);
-	}
-	
-	/*public void put( int interval, Set<Integer> value ) {
+    public boolean contains( int x, int y, int symbol ) {
+        int[] js = get(Util.pair(x, y));
+        if( js == null )
+            return false;
+        for( int ii : js )
+            if( Util.X(ii) == symbol )
+                return true;
+        return false;
+    }
+
+    public int getSymbol( int interval, int index ) {
+        int[] tmp = get(interval);
+        if( tmp == null )
+            return -1;
+        return Util.X(tmp[index]);
+    }
+
+    public int getIntervalMiddle( int interval, int index ) {
+        int[] tmp = get(interval);
+        if( tmp == null )
+            return -1;
+        return Util.Y(tmp[index]);
+    }
+
+    /*public void put( int interval, Set<Integer> value ) {
 		if( value.size()>0 ) {
 			int[] tmp1 = new int[value.size()];
 			int i = 0;
@@ -47,31 +47,31 @@ public class Matrix extends TreeMap<Integer,int[]> {
 			put(interval,tmp1);
 		} 
 	}*/
-	/*public void put( int interval, int[] value ) {
+    /*public void put( int interval, int[] value ) {
 		impl.put(interval,value);
 	}
 
 	public SortedMap<Integer, int[]> subMap(Integer fromKey, Integer toKey) {
 		return impl.subMap(fromKey, toKey);
 	}
-	
+
 	public Set<Integer> keySet() {
 		return impl.keySet();
 	}
-	
+
 	public int[] remove(Object key) {
 		return impl.remove(key);
 	}
-	
+
 	public int size() {
 		return impl.size();
 	}
-	
+
 	public int[] get(Object key) {
 		return impl.get(key);
 	}*/
-	
-	/*	
+
+    /*	
 	public void clear() {
 		impl.clear();
 	}
@@ -131,36 +131,36 @@ public class Matrix extends TreeMap<Integer,int[]> {
 
 	public void putAll(Map<? extends Integer, ? extends int[]> t) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public Collection<int[]> values() {
 		// TODO Auto-generated method stub
 		return null;
 	}
-    */
-		
-	public String toString() throws RuntimeException {
-		StringBuffer ret = new StringBuffer();
-		for( int xy : keySet() ) {
-			int i = Util.X(xy);
-			int j = Util.Y(xy);
-			ret.append("["+i+","+j+")"); 
-			int[] output = get(Util.pair(i, j));
-			if( output ==  null ) {
-				throw new RuntimeException("no value corresponding to the key ["+i+","+j+")"); 
-			}
-			int k = 0;
-			for( int s : output ) {
-				if( k < 5 )
-					ret.append("  "+cyk.allSymbols[Util.X(s)]);
-				else if( k == 5 )
-					ret.append(" ...");
-				k++;
-			}
+     */
 
-			ret.append('\n'); 
-		}
-		return ret.toString();
-	}
+    public String toString() throws RuntimeException {
+        StringBuffer ret = new StringBuffer();
+        for( int xy : keySet() ) {
+            int i = Util.X(xy);
+            int j = Util.Y(xy);
+            ret.append("["+i+","+j+")"); 
+            int[] output = get(Util.pair(i, j));
+            if( output ==  null ) {
+                throw new RuntimeException("no value corresponding to the key ["+i+","+j+")"); 
+            }
+            int k = 0;
+            for( int s : output ) {
+                if( k < 5 )
+                    ret.append("  "+cyk.allSymbols[Util.X(s)]);
+                else if( k == 5 )
+                    ret.append(" ...");
+                k++;
+            }
+
+            ret.append('\n'); 
+        }
+        return ret.toString();
+    }
 }
