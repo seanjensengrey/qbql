8e14d9f0b392228ff285c4aa063344365e3607d1
vadimtro <vadimtro@3fa168a8-ab54-0410-bc16-1f818b10f66c>
2009-09-14 17:14:56 +0000

    Minor amendments & bug fixes.

--- trunk/qbql/program/Run.java
+++ trunk/qbql/program/Run.java
@@ -4,11 +4,14 @@ import qbql.lattice.Database;
 import qbql.util.Util;
 
 public class Run {
+    
     public static void main( String[] args ) throws Exception {
-        String db = Util.readFile(Run.class,"strings.db");
-        String prg = Util.readFile(Run.class,"strings.prg");
+        //String db = Util.readFile(Run.class,"strings.db");
+        //String prg = Util.readFile(Run.class,"strings.prg");
         //String db = Util.readFile(Run.class,"Sims.db");
         //String prg = Util.readFile(Run.class,"Sims.assertions");
+        String db = Util.readFile(Run.class,"Wittgenstein.db");
+        String prg = Util.readFile(Run.class,"Wittgenstein.assertions");
         Database.run(prg, db);    
     }
 
