69f3a1fee7f2906266243585abed0cfb8b681c11
vadimtro <vadimtro@3fa168a8-ab54-0410-bc16-1f818b10f66c>
2009-10-14 06:02:58 +0000

    Parsing predicates completed.

---
+++ qbql/lang/parse/Paths.java
@@ -0,0 +1,38 @@
+package qbql.lang.parse;
+
+import qbql.index.NamedTuple;
+import qbql.lattice.Database;
+import qbql.lattice.Relation;
+import qbql.lattice.Tuple;
+import qbql.parser.ParseNode;
+import qbql.util.Util;
+
+public class Paths {
+
+    // Legend: pos,up,down are intervals [from,to)
+    // Vars ^ [] = [name pos].  
+    // Tokens ^ [] = [txt pos].
+    // Links ^ [] = [up down].
+    // Paths ^ [] = [up down].
+    ParseDb db = null;
+    public Paths( Database d ) {
+        db = (ParseDb)d;       
+    }
+    
+    public Relation down_up( String pos ) throws AssertionError {
+        ParseNode atPos = db.root.locate(Util.X(pos),Util.Y(pos));
+        Relation ret = new Relation(new String[]{"up"});
+        for( ParseNode ancestor : db.root.intermediates(atPos.from,atPos.to) ) {
+           ret.content.add(new Tuple(new Object[]{"["+ancestor.from+","+ancestor.to+")"}));
+        }
+        return ret;      
+    }
+    public Relation up_down( String pos ) {
+        ParseNode atPos = db.root.locate(Util.X(pos),Util.Y(pos));
+        Relation ret = new Relation(new String[]{"down"});
+        for( ParseNode child : atPos.descendants() ) {
+           ret.content.add(new Tuple(new Object[]{"["+child.from+","+child.to+")"}));
+        }
+        return ret;      
+    }
+}
---
+++ qbql/lang/parse/test.prg
@@ -0,0 +1,10 @@
+[pos] "[8,13)" ^ Vars;
+
+[pos] "[8,9)" ^ Tokens;
+[txt] from ^ Tokens;
+
+[up] "[0,6)" ^ Paths;
+[down] "[10,11)" ^ Paths;
+
+[up] "[8,13)" ^ Links;
+[down] "[8,13)" ^ Links;
